11c8a03569e571e7ac8f53acc83b8afcac11de34
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-20 20:31:18 -0300

    Create theme.js

---
+++ rewardsapp/src/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export default extendTheme({
+  colors: {
+    primary: "#0ad4fa",
+    secondary: "#25bbf1",
+  },
+  shadows: {
+    outline: "0",
+  },
+});
