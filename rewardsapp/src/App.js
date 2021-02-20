2b49d616eae3292a81aa308e00ddad8071f8abbd
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-20 20:30:36 -0300

    Update App.js

--- rewardsapp/src/App.js
+++ rewardsapp/src/App.js
@@ -1,8 +1,19 @@
-import * as React from "react";
 import AppRouter from "./routers/AppRouter";
+import { Stack } from "@chakra-ui/react";
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <Stack
+      maxW="1440px"
+      width="100%"
+      margin="auto"
+      bg="#f9f9f9"
+      marginY={{ base: 0, xl: 4 }}
+      spacing={0}
+    >
+      <AppRouter />
+    </Stack>
+  );
 };
 
 export default App;
