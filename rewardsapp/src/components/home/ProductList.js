b48064de2b3a26d7eda78d61f4cdb2a2f45b8935
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-20 20:30:27 -0300

    Create ProductList.js

---
+++ rewardsapp/src/components/home/ProductList.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { Button, Divider, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { api } from "../../api/api";
+import ProductCard from "../products/ProductCard";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    api.getProducts().then((productos) => setProducts(productos));
+  }, []);
+  return (
+    <Stack padding={8} textAlign="center">
+      <Stack isInline>
+        <Text alignSelf="center"> 16 of 32 products</Text>
+        <Stack isInline>
+          <Text alignSelf="center"> Sort By:</Text>
+
+          <Button colorScheme="cyan">Most Recent</Button>
+          <Button colorScheme="cyan">Lower Price</Button>
+          <Button colorScheme="cyan">Highest Price</Button>
+        </Stack>
+      </Stack>
+      <Divider />
+      <Stack>
+        <SimpleGrid columns={{ base: 1, sm: 2, md: 3, xl: 4 }} gap={3}>
+          {products?.map((product) => (
+            <ProductCard product={product} />
+          ))}
+        </SimpleGrid>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default ProductList;
