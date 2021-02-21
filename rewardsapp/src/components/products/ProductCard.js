d04b2b8273afc81af22af8c140a5ce589830e300
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-21 18:07:05 -0300

    Update ProductCard.js

--- rewardsapp/src/components/products/ProductCard.js
+++ rewardsapp/src/components/products/ProductCard.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack, Text, Image, Divider } from "@chakra-ui/react";
+import ProductHover from "./ProductHover";
 
 const ProductCard = ({ product }) => {
   const { id, name, cost, category, img } = product;
+  const [hover, setHover] = useState(false);
   return (
     <Stack
       bg="white"
       boxShadow="2px 2px 4px 0 rgba(0,0,0,0.1)"
+      cursor="pointer"
       height="276px"
       padding={2.5}
       rounded={4}
       spacing={4}
       width="276px"
-      _hover={
-          
-      }
+      transitionDuration="0.4s"
+      transitionProperty="transform"
+      _hover={{
+        transform: "translateY(-0.7em)",
+        borderColor: "1px red",
+      }}
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
     >
+      {hover && <ProductHover productCost={cost} productId={id} />}
       <Stack spacing={0} alignItems="flex-end">
         <Image
           src="../assets/icons/buy-blue.svg"
