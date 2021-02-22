1f81fb4b1f5f595ee55b06af310e483c2586f8d4
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-22 14:45:32 -0300

    Update ProductHover.js

--- rewardsapp/src/components/products/ProductHover.js
+++ rewardsapp/src/components/products/ProductHover.js
@@ -1,20 +1,32 @@
-import { Stack } from "@chakra-ui/react";
 import React from "react";
+import { Stack, Image, Button, Text } from "@chakra-ui/react";
 
-const ProductHover = () => {
+const ProductHover = ({ id, cost }) => {
   return (
     <Stack
       position="absolute"
       top="0"
       left="0"
-      bottom="0"
-      alignItems="center"
-      justifyContent="center"
       width="100%"
+      spacing={4}
+      padding={2.5}
       zIndex="200"
-      opacity=".8"
-      bgGradient="linear(to-b, #0ad4fa 0%, #25bbf1 100%)"
-    ></Stack>
+      height="100%"
+      opacity="0"
+      justifyContent="center"
+      bgGradient="linear(to-b, #0ad4faCC 0%, #25bbf1CC 100%)"
+      transitionDuration="0.4s"
+      transitionProperty="transform"
+      _hover={{
+        opacity: "1",
+      }}
+    >
+      <Stack isInline justifyContent="center" alignItems="center">
+        <Text color="white">{cost}</Text>
+        <Image src="../assets/icons/coin.svg" />
+      </Stack>
+      <Button>Redeem now</Button>
+    </Stack>
   );
 };
 
