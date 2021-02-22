c13c670e7b8c395d93d5fa33ec79e48614a4a2fb
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-22 14:45:30 -0300

    Update ProductCard.js

--- rewardsapp/src/components/products/ProductCard.js
+++ rewardsapp/src/components/products/ProductCard.js
@@ -12,33 +12,35 @@ const ProductCard = ({ product }) => {
       cursor="pointer"
       height="276px"
       padding={2.5}
+      position="relative"
       rounded={4}
-      spacing={4}
+      spacing={0}
       width="276px"
       transitionDuration="0.4s"
       transitionProperty="transform"
       _hover={{
         transform: "translateY(-0.7em)",
-        borderColor: "1px red",
       }}
       onMouseLeave={() => setHover(false)}
       onMouseEnter={() => setHover(true)}
     >
-      {hover && <ProductHover productCost={cost} productId={id} />}
-      <Stack spacing={0} alignItems="flex-end">
-        <Image
-          src="../assets/icons/buy-blue.svg"
-          marginBottom={-10}
-          maxH="42px"
-          maxW="42px"
-          zIndex={30}
-        />
-        <Image src={img.hdUrl} alt={name} />
-        <Divider />
-      </Stack>
-      <Stack textAlign="start" spacing={0}>
-        <Text color="#a3a3a3">{category}</Text>
-        <Text color="#616161">{name}</Text>
+      <ProductHover id={id} cost={cost} />
+      <Stack spacing={4}>
+        <Stack spacing={0} alignItems="flex-end">
+          <Image
+            src="../assets/icons/buy-blue.svg"
+            marginBottom={-10}
+            maxH="42px"
+            maxW="42px"
+            zIndex={30}
+          />
+          <Image src={img.hdUrl} alt={name} />
+          <Divider />
+        </Stack>
+        <Stack textAlign="start" spacing={0}>
+          <Text color="#a3a3a3">{category}</Text>
+          <Text color="#616161">{name}</Text>
+        </Stack>
       </Stack>
     </Stack>
   );
