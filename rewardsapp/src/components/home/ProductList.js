4488583647a6f5bb0f624a2a98066c1104aaed8e
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 13:34:29 -0300

    Update ProductList.js

--- rewardsapp/src/components/home/ProductList.js
+++ rewardsapp/src/components/home/ProductList.js
@@ -1,36 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { Button, Divider, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { Divider, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 import { api } from "../../api/api";
+import { getSort } from "../../helpers/getSort";
+
+import SortButtons from "./SortButtons";
 import ProductCard from "../products/ProductCard";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [sortSelected, setSortSelected] = useState("recent");
+
   useEffect(() => {
-    api.getProducts().then((productos) => setProducts(productos));
+    api
+      .getProducts()
+      .then((productos) => setProducts(getSort(productos, sortSelected)));
   }, []);
+
+  const handleSortChange = (sortMethod) => {
+    setSortSelected(sortMethod);
+    setProducts(getSort(products, sortMethod));
+  };
+
   return (
     <Stack padding={8} spacing={10} textAlign="center">
       <Stack isInline>
         <Text alignSelf="center"> 16 of 32 products</Text>
-        <Stack isInline>
-          <Text alignSelf="center"> Sort By:</Text>
-
-          <Button colorScheme="cyan">Most Recent</Button>
-          <Button colorScheme="cyan">Lower Price</Button>
-          <Button colorScheme="cyan">Highest Price</Button>
-        </Stack>
+        <SortButtons
+          sortSelected={sortSelected}
+          handleChange={handleSortChange}
+        />
       </Stack>
       <Divider />
-      <Stack>
-        <SimpleGrid
-          columns={{ base: 1, sm: 2, md: 3, xl: 4 }}
-          gap={8}
-          alignSelf="center"
-        >
-          {products?.map((product) => (
-            <ProductCard product={product} />
-          ))}
-        </SimpleGrid>
+
+      <SimpleGrid
+        columns={{ base: 1, sm: 2, md: 3, xl: 4 }}
+        gap={8}
+        alignSelf="center"
+      >
+        {products?.map((product) => (
+          <ProductCard product={product} key={product._id} />
+        ))}
+      </SimpleGrid>
+
+      <Divider />
+      <Stack isInline justify="space-between">
+        <Text alignSelf="center"> 16 of 32 products</Text>
       </Stack>
     </Stack>
   );
