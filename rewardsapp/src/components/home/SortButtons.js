f83e45a51870cc8ca62b9d30bdd1a682def3b02a
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 13:34:26 -0300

    Create SortButtons.js

---
+++ rewardsapp/src/components/home/SortButtons.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Stack, Text, Button } from "@chakra-ui/react";
+
+const SortButtons = ({ sortSelected, handleChange }) => {
+  return (
+    <Stack isInline>
+      <Text alignSelf="center"> Sort By:</Text>
+
+      <Button
+        bg={sortSelected === "recent" ? "primary" : "#ededed"}
+        color={sortSelected === "recent" ? "white" : "#a3a3a3"}
+        rounded={999}
+        _hover={{ backgroundColor: "secondary", color: "white" }}
+        onClick={() => handleChange("recent")}
+      >
+        Most Recent
+      </Button>
+      <Button
+        bg={sortSelected === "lowest" ? "primary" : "#ededed"}
+        color={sortSelected === "lowest" ? "white" : "#a3a3a3"}
+        rounded={999}
+        _hover={{ backgroundColor: "secondary", color: "white" }}
+        onClick={() => handleChange("lowest")}
+      >
+        Lowest Price
+      </Button>
+      <Button
+        bg={sortSelected === "highest" ? "primary" : "#ededed"}
+        color={sortSelected === "highest" ? "white" : "#a3a3a3"}
+        rounded={999}
+        _hover={{ backgroundColor: "secondary", color: "white" }}
+        onClick={() => handleChange("highest")}
+      >
+        Highest Price
+      </Button>
+    </Stack>
+  );
+};
+
+export default SortButtons;
