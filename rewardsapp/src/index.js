143b190172eac1ea2de2ce6faba788ed95163342
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 18:09:11 -0300

    Update index.js

--- rewardsapp/src/index.js
+++ rewardsapp/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import theme from "./theme";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import { UserProvider } from "./context/UserContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider resetCSS theme={theme}>
-      <App />
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider resetCSS theme={theme}>
+        <App />
+      </ChakraProvider>
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
