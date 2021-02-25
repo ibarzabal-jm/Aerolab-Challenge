3c710c0162a13b6eae318629e6d888fea4330d7d
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 13:33:51 -0300

    Update AppRouter.js

--- rewardsapp/src/routers/AppRouter.js
+++ rewardsapp/src/routers/AppRouter.js
@@ -8,12 +8,14 @@ import {
 import Navbar from "../components/UI/Navbar";
 import Home from "../pages/Home";
 import Footer from "../components/UI/Footer";
+import Redeems from "../pages/Redeems";
 
 const AppRouter = () => {
   return (
     <Router>
       <Navbar />
       <Switch>
+        <Route path="/redeems" component={Redeems} />
         <Route path="/home">
           <Home />
         </Route>
