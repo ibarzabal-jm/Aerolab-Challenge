de411284bf936ea1e77b6b07b3c45628f75378a6
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 13:34:12 -0300

    Update ProductCard.js

--- rewardsapp/src/components/products/ProductCard.js
+++ rewardsapp/src/components/products/ProductCard.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import { Stack, Text, Image, Divider } from "@chakra-ui/react";
+import React, { useState, useContext } from "react";
+import { Stack, Text, Image, Divider, TagLabel, Tag } from "@chakra-ui/react";
+import { UserContext } from "../../context/UserContext";
 import ProductHover from "./ProductHover";
 
 const ProductCard = ({ product }) => {
-  const { id, name, cost, category, img } = product;
+  const {
+    user: { points },
+  } = useContext(UserContext);
+
+  const { _id, name, cost, category, img } = product;
   const [hover, setHover] = useState(false);
   return (
     <Stack
       bg="white"
       boxShadow="2px 2px 4px 0 rgba(0,0,0,0.1)"
-      cursor="pointer"
       height="276px"
       padding={2.5}
       position="relative"
@@ -24,16 +28,30 @@ const ProductCard = ({ product }) => {
       onMouseLeave={() => setHover(false)}
       onMouseEnter={() => setHover(true)}
     >
-      <ProductHover id={id} cost={cost} />
+      {points >= cost && <ProductHover id={_id} cost={cost} />}
       <Stack spacing={4}>
         <Stack spacing={0} alignItems="flex-end">
-          <Image
-            src="../assets/icons/buy-blue.svg"
-            marginBottom={-10}
-            maxH="42px"
-            maxW="42px"
-            zIndex={30}
-          />
+          {points >= cost ? (
+            <Image
+              src="../assets/icons/buy-blue.svg"
+              marginBottom={-10}
+              maxH="42px"
+              maxW="42px"
+              opacity={hover ? 0 : 1}
+              zIndex={100}
+            />
+          ) : (
+            <Tag bg="#616161CC" marginBottom={-10} rounded={999} zIndex={100}>
+              <TagLabel>You need {cost - points} </TagLabel>
+              <Image
+                src="../assets/icons/coin.svg"
+                ml={1}
+                mt={1}
+                maxW="32px"
+                maxH="32px"
+              />
+            </Tag>
+          )}
           <Image src={img.hdUrl} alt={name} />
           <Divider />
         </Stack>
