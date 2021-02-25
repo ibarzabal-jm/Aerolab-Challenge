6126d59be7b31ecd4ec50e5969b0fa2cd5db0603
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 13:34:15 -0300

    Update Header.js

--- rewardsapp/src/components/home/Header.js
+++ rewardsapp/src/components/home/Header.js
@@ -4,12 +4,12 @@ import React from "react";
 const Header = () => {
   return (
     <Flex
-      background="url(../assets/header-x1.png) center right"
+      background="url(../assets/header.png) center right"
       backgroundSize="cover"
-      minHeight="412px"
+      minHeight="300px"
       width="100%"
     >
-      <Heading color="white" alignSelf="flex-end" padding={6}>
+      <Heading color="white" fontSize="64px" alignSelf="flex-end" padding={6}>
         Electronics
       </Heading>
     </Flex>
