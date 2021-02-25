1fd6c2fb6459064c87cff83a5455ea598fb350aa
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 18:09:09 -0300

    Update Home.js

--- rewardsapp/src/pages/Home.js
+++ rewardsapp/src/pages/Home.js
@@ -3,7 +3,6 @@ import { Stack } from "@chakra-ui/react";
 import Header from "../components/home/Header";
 import ProductList from "../components/home/ProductList";
 
-
 const Home = () => {
   return (
     <Stack bg="#f9f9f9">
