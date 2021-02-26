cee65f41b6e2e27020730daf17c77164864b34dd
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-25 21:49:34 -0300

    Update Home.js

--- rewardsapp/src/pages/Home.js
+++ rewardsapp/src/pages/Home.js
@@ -1,13 +1,30 @@
-import React from "react";
-import { Stack } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { CircularProgress, Stack } from "@chakra-ui/react";
 import Header from "../components/home/Header";
 import ProductList from "../components/home/ProductList";
+import { api } from "../api/api";
 
 const Home = () => {
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api.getProducts().then((products) => {
+      setProducts(products);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <Stack bg="#f9f9f9">
       <Header />
-      <ProductList />
+      {loading ? (
+        <Stack alignItems="center" justifyContent="center" paddingY={12}>
+          <CircularProgress color="secondary" />
+        </Stack>
+      ) : (
+        <ProductList products={products} />
+      )}
     </Stack>
   );
 };
