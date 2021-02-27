d6481ac5cfbd5ad4e9cc0996cad88d00f4635982
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-26 23:22:09 -0300

    Move User Provider

--- rewardsapp/src/App.js
+++ rewardsapp/src/App.js
@@ -1,33 +1,22 @@
 import AppRouter from "./routers/AppRouter";
-import { Stack, Image, Heading } from "@chakra-ui/react";
-import { useContext } from "react";
-import { UserContext } from "./context/UserContext";
+import { Stack } from "@chakra-ui/react";
+import { UserProvider } from "./context/UserContext";
 
 const App = () => {
-  const { user } = useContext(UserContext);
-
-  return user.name === "" ? (
-    <Stack
-      bg="primary"
-      justify="center"
-      align="center"
-      height="100vh"
-      width="100vw"
-    >
-      <Image src="../assets/logo.svg" w={20} h="{20" />
-      <Heading textAlign="center">Loading...</Heading>
-    </Stack>
-  ) : (
-    <Stack
-      maxW="1440px"
-      width="100%"
-      margin="auto"
-      bg="#f9f9f9"
-      m={{ base: 0, xl: 4 }}
-      spacing={0}
-    >
-      <AppRouter />
-    </Stack>
+  return (
+    <UserProvider>
+      <Stack
+        maxW="1440px"
+        width="100%"
+        margin="auto"
+        bg="#f9f9f9"
+        m={{ base: 0, xl: 4 }}
+        spacing={0}
+        minH="100vh"
+      >
+        <AppRouter />
+      </Stack>
+    </UserProvider>
   );
 };
 
--- rewardsapp/src/index.js
+++ rewardsapp/src/index.js
@@ -5,15 +5,12 @@ import App from "./App";
 import theme from "./theme";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { UserProvider } from "./context/UserContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
-      <ChakraProvider resetCSS theme={theme}>
-        <App />
-      </ChakraProvider>
-    </UserProvider>
+    <ChakraProvider resetCSS theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
