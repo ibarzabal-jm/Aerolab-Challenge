c79f770aabff0087f060caf1824f5212093b9229
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-26 23:24:36 -0300

    Update ModalCoins.js

--- rewardsapp/src/components/UI/ModalCoins.js
+++ rewardsapp/src/components/UI/ModalCoins.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Image,
@@ -14,8 +14,10 @@ import {
 } from "@chakra-ui/react";
 
 const ModalCoins = ({ userPoints, handleAddCoins }) => {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const poinstToAdd = [1000, 5000, 7500];
+  const [isOpen, setIsOpen] = useState(false);
   const open = () => setIsOpen(!isOpen);
+
   return (
     <Popover>
       <PopoverTrigger>
@@ -39,54 +41,25 @@ const ModalCoins = ({ userPoints, handleAddCoins }) => {
         </PopoverHeader>
         <PopoverBody>
           <Stack isInline align="center">
-            <Button
-              rounded={99}
-              value="1000"
-              color="#616161"
-              letterSpacing="-0.15px"
-              onClick={handleAddCoins}
-            >
-              {1000}
-              <Image
-                src="../assets/icons/coin.svg"
-                ml={1}
-                mt={1}
-                width={6}
-                cursor="default"
-              />
-            </Button>
-            <Button
-              rounded={99}
-              value="5000"
-              color="#616161"
-              letterSpacing="-0.15px"
-              onClick={handleAddCoins}
-            >
-              {5000}
-              <Image
-                src="../assets/icons/coin.svg"
-                ml={1}
-                mt={1}
-                width={6}
-                cursor="default"
-              />
-            </Button>
-            <Button
-              rounded={99}
-              value="7500"
-              color="#616161"
-              letterSpacing="-0.15px"
-              onClick={handleAddCoins}
-            >
-              {7500}
-              <Image
-                src="../assets/icons/coin.svg"
-                ml={1}
-                mt={1}
-                width={6}
-                cursor="default"
-              />
-            </Button>
+            {poinstToAdd.map((point) => (
+              <Button
+                key={point}
+                rounded={99}
+                value={point}
+                color="#616161"
+                letterSpacing="-0.15px"
+                onClick={() => handleAddCoins(point)}
+              >
+                {point}
+                <Image
+                  src="../assets/icons/coin.svg"
+                  ml={1}
+                  mt={1}
+                  width={6}
+                  cursor="default"
+                />
+              </Button>
+            ))}
           </Stack>
         </PopoverBody>
       </PopoverContent>
