8230774cd533aec4cd319cab8d0c68600f69e414
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-28 12:16:21 -0300

    refactoring code

--- rewardsapp/src/App.js
+++ rewardsapp/src/App.js
@@ -1,31 +1,25 @@
 import AppRouter from "./routers/AppRouter";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import { Stack } from "@chakra-ui/react";
 
-import { UserProvider } from "./context/UserContext";
 import Navbar from "./components/UI/Navbar";
 import Footer from "./components/UI/Footer";
 
 const App = () => {
   return (
-    <UserProvider>
-      <Stack
-        maxW="1440px"
-        width="100%"
-        margin="auto"
-        bg="#f9f9f9"
-        m={{ base: 0, xl: 4 }}
-        spacing={0}
-        minH="100vh"
-      >
-        <Router>
-          <Navbar />
-          <AppRouter />
-          <Footer />
-        </Router>
-      </Stack>
-    </UserProvider>
+    <Stack
+      maxW="1440px"
+      width="100%"
+      margin="auto"
+      bg="#f9f9f9"
+      m={{ base: 0, xl: 4 }}
+      spacing={0}
+      minH="100vh"
+    >
+      <Navbar />
+      <AppRouter />
+      <Footer />
+    </Stack>
   );
 };
 
--- rewardsapp/src/index.js
+++ rewardsapp/src/index.js
@@ -5,11 +5,17 @@ import App from "./App";
 import theme from "./theme";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { UserProvider } from "./context/UserContext";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider resetCSS theme={theme}>
-      <App />
+      <UserProvider>
+        <Router>
+          <App />
+        </Router>
+      </UserProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
