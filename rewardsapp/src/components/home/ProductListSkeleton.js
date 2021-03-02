1f6e2ffa1108aed9e7decbba2f4eaf097e842e7b
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-02 03:38:12 -0300

    add animation to skeleton, update more realistic

--- rewardsapp/src/components/home/ProductListSkeleton.js
+++ rewardsapp/src/components/home/ProductListSkeleton.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Stack, Grid, Skeleton, Divider } from "@chakra-ui/react";
+import {
+  Stack,
+  Grid,
+  Skeleton,
+  Divider,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 const ProductListSkeleton = ({ productsPerPage }) => {
   return (
@@ -13,7 +21,38 @@ const ProductListSkeleton = ({ productsPerPage }) => {
         justifyItems="center"
       >
         {[...Array(productsPerPage)].map((skelet, i) => (
-          <Skeleton key={i} height="276px" width="276px"></Skeleton>
+          <motion.div
+            animate="visible"
+            key={i}
+            layout
+            custom={i}
+            initial="hidden"
+            variants={{
+              hidden: { opacity: 0, y: 100 },
+              visible: (index) => ({
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: index * 0.06,
+                },
+              }),
+            }}
+          >
+            <Stack
+              bg="white"
+              boxShadow="2px 2px 4px 0 rgba(0,0,0,0.1)"
+              padding={2.5}
+              height="276px"
+              width="276px"
+            >
+              <SkeletonCircle alignSelf="flex-end" />
+              <Skeleton>
+                <Stack w="240px" h="160px"></Stack>
+              </Skeleton>
+              <Divider />
+              <SkeletonText mt="4" noOfLines={2} spacing="4" w={28} />
+            </Stack>
+          </motion.div>
         ))}
       </Grid>
     </Stack>
